14f5ae82d603c6cb201ab45bb23aeb8fdbe89e73
Jambo-Juice <mareep.scarlet@gmail.com>
2026-09-17 19:25:45 +0100

    Initial commit

---
+++ src/main/java/net/jetsetter/perfectpocket/PerfectPocketClient.java
@@ -0,0 +1,11 @@
+package net.jetsetter.perfectpocket;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PerfectPocketClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+
+    }
+}
---
+++ src/main/java/net/jetsetter/perfectpocket/PerfectPocketDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jetsetter.perfectpocket;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PerfectPocketDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
